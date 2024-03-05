de3a648b866b27165a8f6ee5ce03c20168ff3c19
PixelCombats2 <145314654+hoycmd@users.noreply.github.com>
2024-03-05 17:59:37 +0700

    Update client.js

--- client.js
+++ client.js
@@ -1,1 +1,19 @@
-
+{
+  "Name": "<color=orange>ФАРМ СУНДУКОВ И МЕДАЛЕЙ (API 2.0)</a>",
+  "Description": "<color=red>Для работа фарма нада 2 человек|ВЗЯЛ ИДЕЮ У:just_qstn|Абнова API 2.0|удачи;)</a>",
+  "StartClientVersion": 334,
+  "MapFilter": {
+    "IsBlank": false,
+    "HasParcourEnd": false
+  },
+  "MapLists": [
+    {
+      "MapListId": 324,
+      "Name": "OFFICIAL MAPS"
+    },
+    {
+      "MapListId": 581
+    }
+  ],
+  "Parameters": {}
+}
